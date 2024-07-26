8c7c4c7f12b948d7e92d650350676f02c4b5fd2f
haruka wada <022.wadaha@gmail.com>
2024-07-26 10:52:22 +0900

    カテゴリー作成機能の追加

--- src/resources/views/category.blade.php
+++ src/resources/views/category.blade.php
@@ -11,8 +11,8 @@
         {{ session('message') }}
     </div>
     @endif
-    @if(count($errors) > 0)
-    <div class="todo__alert--danger">
+    @if($errors->any())
+    <div class="category__alert--danger">
         <ul>
             @foreach($errors->all() as $error)
             <li>{{ $error }}</li>
@@ -22,9 +22,10 @@
     @endif
 </div>
 <div class="category__content">
-    <form class="create-form">
+    <form class="create-form" action="/categories" method="post">
+        @csrf
         <div class="create-form__item">
-            <input class="create-form__item-input" type="text">
+            <input class="create-form__item-input" type="text" name="name" value="{{ old('name') }}">
         </div>
         <div class="create-form__button">
             <button class="create-form__button-submit" type="submit">作成</button>
@@ -40,7 +41,7 @@
                 <td class="category-table__item">
                     <form class="update-form">
                         <div class="update-form__item">
-                            <input class="update-form__item-input" type="text">
+                            <input class="update-form__item-input" type="text" value="{{ $category['name'] }}">
                         </div>
                         <div class="update-form__button">
                             <button class="update-form__button-submit" type="submit">更新</button>
